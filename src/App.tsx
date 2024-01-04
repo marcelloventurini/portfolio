3491c96fa3b0b6b722c192d542f0e588ad9d59c8
Marcello Venturini <marcelloventurini@gmail.com>
2024-01-04 16:49:38 -0300

    feat: add base routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import Footer from 'components/Footer/Footer'
 import Header from 'components/Header/Header'
 import Home from 'pages/Home/Home'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.scss'
 
 function App() {
   return (
     <>
-      <Header />
-      <main className='container'>
-        <Home />
-      </main>
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <main className='container'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+          </Routes>
+        </main>
+        <Footer />
+      </BrowserRouter>
     </>
   )
 }
