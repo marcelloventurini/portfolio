9ea637823b7049e38e9b7437e6d0fff9abbb6c89
Marcello Venturini <marcelloventurini@gmail.com>
2024-01-04 12:03:48 -0300

    feat: create footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.scss'
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p className={styles.footer__text}>design & código por Marcello Venturini</p>
+    </footer>
+  )
+}
+
+export default Footer
